882d134afc01fc540b6f3b9659c8eefbae70656e
Jasper Mi <darmi19@163.com>
2020-03-03 19:09:30 +0800

    添加第四章复习代码

---
+++ test/chapter_04/test4.py
@@ -0,0 +1,7 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+# 复制列表
+friend_foods = my_foods[:]
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print(my_foods)
+print(friend_foods)
