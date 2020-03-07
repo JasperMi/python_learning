221c7de9dced9056224179b4f6a116d760b61312
Jasper Mi <darmi19@163.com>
2020-03-07 18:38:52 +0800

    添加复习第九章代码练习

---
+++ test/chapter_09/test1.py
@@ -0,0 +1,7 @@
+# 导入单个类：from car import Car
+
+# 从一个模块中导入多个类：from car import Car, ElectricCar
+
+# 导入整个模块：import car，使用module_name.class_name访问需要的类
+
+# 导入模块中的所有类：from module_name import *
---
+++ test/chapter_09/test2.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+
+# OrderedDict记录键值对的添加顺序
+favorite_languages = OrderedDict()
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + language.title() + ".")
